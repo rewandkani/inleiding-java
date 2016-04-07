2bcba85c98b3add981ab1ec287ef1f867eb57a09
Rewand <rhussein@roc-dev.com>
2016-04-07 11:46:29 +0200

    Hoofdstuk 6

---
+++ src/h06/Opdracht6_3.java
@@ -0,0 +1,21 @@
+package h06;
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class Opdracht6_3 extends Applet {
+
+	
+	public void init() { 
+		
+		setBackground(Color.white);
+
+	}
+	
+	public void paint (Graphics g) {
+		g.drawString(""+(2000000000+1234567890),50,50);
+		
+	}
+	
+}
---
+++ src/h06/Opdracht6_4.java
@@ -0,0 +1,34 @@
+package h06;
+
+
+import java.applet.*;
+import java.awt.*;
+
+	
+
+	@SuppressWarnings("serial")
+	public class Opdracht6_4 extends Applet {
+
+		
+		int gemiddelde = (int) ((5.9+6.3+6.9)/3*10);
+		double einde = (double) gemiddelde/10;
+		
+		
+
+	public void init() { 
+		
+		setBackground(Color.white);
+
+	}
+
+	public void paint (Graphics g) {
+		int x = 50;
+		int y =70;
+		
+		g.drawString("test " + einde, 100, 100);
+		
+	g.drawString("het gemiddelde is : "+(double)	(int)	(	(5.9+6.3+6.9)	/3*10)	/10 ,x,y);
+
+	}
+	
+}
